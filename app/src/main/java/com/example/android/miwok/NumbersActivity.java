51f0bd8a65481eee9b753e70c3304cacfbc54c02
Faithful-Mind <lpyy15@gmail.com>
2019-04-16 23:14:28 +0800

    Add numbers strings

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        String[] words = {"one", "two", "three", "four", "five", "six", "seven",
+            "eight", "nine", "ten"};
+        int wordsLength = words.length;
+        for (int i = 0; i < wordsLength; i++) {
+            Log.v("NumbersActivity", "Word at index "+ i + ": " + words[i]);
+        }
     }
 }
