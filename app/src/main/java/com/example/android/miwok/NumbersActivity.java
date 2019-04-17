9699e9cf5ef4682068fb37a05a9da73086e5bfe1
Faithful-Mind <lpyy15@gmail.com>
2019-04-17 22:06:52 +0800

    Add numbers in a loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,8 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,9 +18,12 @@ public class NumbersActivity extends AppCompatActivity {
 
         List<String> words = Arrays.asList("one", "two", "three", "four", "five", "six", "seven",
             "eight", "nine", "ten");
-        int wordsLength = words.size();
-        for (int i = 0; i < wordsLength; i++) {
-            Log.v("NumbersActivity", "Word at index "+ i + ": " + words.get(i));
+
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.root_view_numbers);
+        for (int index = 0; index < words.size(); index++) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
         }
     }
 }
