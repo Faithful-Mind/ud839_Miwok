e2cd579f649b0acda50ff9ae1f3f3de5455a97d6
Faithful-Mind <lpyy15@gmail.com>
2019-04-18 05:02:40 +0800

    Show numbers in a ListView

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.Arrays;
@@ -19,11 +21,9 @@ public class NumbersActivity extends AppCompatActivity {
         List<String> words = Arrays.asList("one", "two", "three", "four", "five", "six", "seven",
             "eight", "nine", "ten");
 
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.root_view_numbers);
-        for (int index = 0; index < words.size(); index++) {
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(index));
-            rootView.addView(wordView);
-        }
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
     }
 }
