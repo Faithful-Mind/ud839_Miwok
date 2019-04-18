f689d74105a02213aa41a3260968a973a42a0f06
Faithful-Mind <lpyy15@gmail.com>
2019-04-18 21:39:34 +0800

    Add images to Word class

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -9,6 +9,8 @@ public class Word {
     private String mDefaultTranslation;
     /** Miwok translation for the word */
     private String mMiwokTranslation;
+    /** Image resource ID for the word */
+    private int mImageResId;
 
     /**
      * Create a new {@link Word} object.
@@ -23,6 +25,20 @@ public class Word {
     }
 
     /**
+     * Create a new {@link Word} object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation   is the word in the Miwok language
+     * @param imageResId         is the drawable resource ID for the image associated with the word
+     */
+    public Word(String defaultTranslation, String miwokTranslation, int imageResId) {
+        this.mDefaultTranslation = defaultTranslation;
+        this.mMiwokTranslation = miwokTranslation;
+        this.mImageResId = imageResId;
+    }
+
+    /**
      * Get the default translation of the word.
      */
     public String getDefaultTranslation() {
@@ -34,5 +50,9 @@ public class Word {
      */
     public String getMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    public int getImageResId() {
+        return mImageResId;
     }
 }
