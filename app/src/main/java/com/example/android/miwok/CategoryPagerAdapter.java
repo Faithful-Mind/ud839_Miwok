eb4370dab268dc52778b88d006d776649c3fa95f
Faithful-Mind <lpyy15@gmail.com>
2019-04-24 17:05:55 +0800

    Add tabs to ViewPager

--- app/src/main/java/com/example/android/miwok/CategoryPagerAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryPagerAdapter.java
@@ -1,14 +1,20 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 public class CategoryPagerAdapter extends FragmentPagerAdapter {
+    /** Context of the app */
+    private Context mContext;
 
-    public CategoryPagerAdapter(FragmentManager fm) {
+
+    public CategoryPagerAdapter(Context context, FragmentManager fm) {
         super(fm);
+        this.mContext = context;
     }
+
 
     @Override
     public Fragment getItem(int position) {
@@ -24,5 +30,16 @@ public class CategoryPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0: return mContext.getString(R.string.category_numbers);
+            case 1: return mContext.getString(R.string.category_family);
+            case 2: return mContext.getString(R.string.category_colors);
+            default:
+            case 3: return mContext.getString(R.string.category_phrases);
+        }
     }
 }
--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -16,6 +16,7 @@
 package com.example.android.miwok;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -31,7 +32,10 @@ public class MainActivity extends AppCompatActivity {
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         // Create an adapter that knows which fragment should be shown on each page
         CategoryPagerAdapter adapter =
-                new CategoryPagerAdapter(getSupportFragmentManager());
+                new CategoryPagerAdapter(this, getSupportFragmentManager());
         viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
